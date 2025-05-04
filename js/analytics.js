24a9f814b6c4c91f30a85d1d8c231ef06656a512
BrandXX <v_5carrolljo@yahoo.com>
2025-05-04 00:41:56 -0700

    Activate Google Analytics with Measurement ID G-358YBL56YW

--- js/analytics.js
+++ js/analytics.js
@@ -1,14 +1,10 @@
 // Google Analytics 4 implementation
-// Replace 'G-XXXXXXXXXX' with your actual Google Analytics 4 Measurement ID
 
 // Google tag (gtag.js)
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-XXXXXXXXXX');
+gtag('config', 'G-358YBL56YW');
 
-// Note: To complete setup:
-// 1. Create a Google Analytics 4 property at https://analytics.google.com/
-// 2. Get your Measurement ID (format: G-XXXXXXXXXX)
-// 3. Replace the placeholder above with your actual Measurement ID
-// 4. Uncomment the script tag in the HTML files
+// Measurement ID: G-358YBL56YW
+// To modify this configuration in the future, update the ID above
